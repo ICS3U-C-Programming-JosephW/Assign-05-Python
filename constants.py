ae530777cc5b1ca98975d76b659588bc803500fd
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-05-19 13:33:24 -0400

    Added constants.py, header comments, and a singular constant.

---
+++ constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: May 19, 2025
+# This module contains constants.
+
+# Create an associative array (dictionary)
+# for information based on friction type.
+FRICTION_INFO = {
+    "static": "static friction (stiction)",
+    "kinetic": "kinetic friction (dynamic friction)",
+}
