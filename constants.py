607217df901208d8220eb999166ea9f16a9cefee
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-05-19 21:17:04 -0400

    Added greeting and started do-while in friction_calc.py. Added text codes in constants.py.

--- constants.py
+++ constants.py
@@ -3,9 +3,18 @@
 # Date: May 19, 2025
 # This module contains constants.
 
-# Create an associative array (dictionary)
-# for information based on friction type.
+# Create a constant dictionary (associative array)
+# for information based on the chosen friction type.
 FRICTION_INFO = {
     "static": "static friction (stiction)",
     "kinetic": "kinetic friction (dynamic friction)",
 }
+
+# For text escape codes.
+LIGHT_RED = "\033[1;31m"
+LIGHT_GREEN = "\033[1;32m"
+LIGHT_BLUE = "\033[1;34m"
+LIGHT_PURPLE = "\033[1;35m"
+LIGHT_CYAN = "\033[1;36m"
+DARK_GRAY = "\033[1;30m"
+WHITE = "\033[0m"
--- friction_calc.py
+++ friction_calc.py
@@ -5,6 +5,9 @@
 # kinetic friction given the friction type,
 # coefficient of friction, and normal force.
 
+# Import the constants module for useful constants.
+import constants
+
 
 # Define a function to calculate the
 # resulting frictional force.
@@ -16,7 +19,16 @@ def calculate_friction(friction_coefficient, normal_force):
 
 # Define the main function.
 def main():
-    pass
+    # Display the greeting message to the user.
+    print(f"\n{constants.LIGHT_GREEN}Hello, user! This program will calculate the frictional "
+    f"force based on the entered static or kinetic coefficient of friction and normal force."
+    f"{constants.WHITE}\n")
+
+    # Construct an infinite while loop.
+    while True:
+        # Get the friction type from the user,
+        # which can be static or kinetic.
+        user_frict_type = input("Enter the friction type (static or kinetic): ")
 
 
 # Check if the special name of the file is __main__.
